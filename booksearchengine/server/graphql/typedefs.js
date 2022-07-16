186d9b3ae648263d7803826115be54e24071a30f
Chris Porter <cap8095@gmail.com>
2022-07-16 13:47:54 -0400

    working towards getting the signup to work

---
+++ booksearchengine/server/graphql/typedefs.js
@@ -0,0 +1,41 @@
+const { gql } = require("apollo-server-express");
+const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    savedBooks: [Book]
+    bookCount: Int
+  }
+  type Book {
+    bookId: String
+    title: String
+    authors: [String]
+    description: String
+    image: String
+    link: String
+  }
+  type Auth {
+    token: ID!
+    user: User
+  }
+  type Query {
+    getSingleUser(usernmae: String!): User
+    me: User
+  }
+  input BookInput {
+    bookId: String
+    title: String
+    authors: [String]
+    description: String
+    image: String
+    #link: String
+  }
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(input: BookInput!): User
+    removeBook(bookId: String!): User
+  }
+`;
+module.exports = typeDefs;
